6ba8e89fb9a9ca9e672f357303a1ea5b25822b34
zjun <565061763@qq.com>
2024-11-27 15:15:57 +0800

    Tag_img

--- app/src/main/java/com/sd/demo/compose/html/Common.kt
+++ app/src/main/java/com/sd/demo/compose/html/Common.kt
@@ -5,9 +5,9 @@ import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,67 +16,86 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.sd.lib.compose.html.ComposeHtml
 import com.sd.lib.compose.html.Factory
 import com.sd.lib.compose.html.rememberComposeHtml
-import org.jsoup.nodes.Element
+import com.sd.lib.compose.html.tags.Tag_img
 
 @Composable
 fun AppTextView(
    modifier: Modifier = Modifier,
    html: String,
 ) {
-   val composeHtml = rememberComposeHtml().apply {
-      Factory("img") { AppTag_img() }
-   }
-
-   val annotated = remember(composeHtml, html) { composeHtml.parse(html) }
-   val inlineContent by composeHtml.inlineContentFlow.collectAsStateWithLifecycle()
-
-   Text(
+   BoxWithConstraints(
       modifier = modifier,
-      text = annotated,
-      color = Color.Black,
-      fontSize = 14.sp,
-      lineHeight = 18.sp,
-      inlineContent = inlineContent,
-   )
+   ) {
+      val density = LocalDensity.current
+      val lineHeight = 18.sp
+
+      val composeHtml = rememberComposeHtml().apply {
+         Factory("img") {
+            AppTag_img(
+               density = density,
+               maxWidth = maxWidth,
+               lineHeight = lineHeight,
+            )
+         }
+      }
+
+      val annotated = remember(composeHtml, html) { composeHtml.parse(html) }
+      val inlineContent by composeHtml.inlineContentFlow.collectAsStateWithLifecycle()
+
+      Text(
+         text = annotated,
+         color = Color.Black,
+         fontSize = 14.sp,
+         lineHeight = lineHeight,
+         inlineContent = inlineContent,
+      )
+   }
 }
 
-private class AppTag_img : ComposeHtml.Tag() {
-   override fun elementStart(builder: AnnotatedString.Builder, element: Element) {
-      val src = element.attr("src")
-      val alt = element.attr("alt")
-      builder.appendInlineContent(id = src)
+private class AppTag_img(
+   density: Density,
+   maxWidth: Dp,
+   lineHeight: TextUnit,
+) : Tag_img(
+   density = density,
+   maxWidth = maxWidth,
+   lineHeight = lineHeight,
+) {
+   override fun addImgContent(src: String, alt: String, placeholder: Placeholder) {
       addInlineContent(
          id = src,
-         placeholderWidth = 36.sp,
-         placeholderHeight = 36.sp,
-         placeholderVerticalAlign = PlaceholderVerticalAlign.Center,
-         content = {
-            Box(
-               contentAlignment = Alignment.Center,
+         placeholder = placeholder.copy(
+            width = 48.sp,
+            height = 48.sp,
+         ),
+      ) {
+         Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+               .fillMaxSize()
+               .background(Color.Green.copy(0.5f))
+         ) {
+            Image(
+               painter = painterResource(R.drawable.cn),
+               contentDescription = alt,
+               contentScale = ContentScale.FillWidth,
                modifier = Modifier
-                  .fillMaxSize()
-                  .background(Color.Green.copy(0.5f))
-            ) {
-               Image(
-                  painter = painterResource(R.drawable.cn),
-                  contentDescription = alt,
-                  contentScale = ContentScale.FillWidth,
-                  modifier = Modifier
-                     .background(Color.Green.copy(0.2f))
-                     .fillMaxWidth(),
-               )
-            }
-         },
-      )
+                  .background(Color.Green.copy(0.2f))
+                  .fillMaxWidth(),
+            )
+         }
+      }
    }
 }
 
