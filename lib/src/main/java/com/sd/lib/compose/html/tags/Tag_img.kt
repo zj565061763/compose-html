830a121f4f77b297248400fb278a912a193f19a1
zjun <565061763@qq.com>
2024-11-27 15:09:41 +0800

    Tag_img

---
+++ lib/src/main/java/com/sd/lib/compose/html/tags/Tag_img.kt
@@ -0,0 +1,59 @@
+package com.sd.lib.compose.html.tags
+
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import com.sd.lib.compose.html.ComposeHtml
+import org.jsoup.nodes.Element
+
+abstract class Tag_img(
+   private val density: Density,
+   private val maxWidth: Dp,
+   private val lineHeight: TextUnit,
+) : ComposeHtml.Tag() {
+   override fun elementStart(builder: AnnotatedString.Builder, element: Element) {
+      val src = element.attr("src")
+
+      val maxWidthPx = density.run { maxWidth.toPx() }
+      val lineHeightPx = density.run { lineHeight.toPx() }
+
+      val width = (element.attr("width").toFloatOrNull() ?: lineHeightPx).coerceIn(0f, maxWidthPx)
+      if (width <= 0f) {
+         builder.appendInlineContent(id = src)
+         return
+      }
+
+      var placeholderVerticalAlign = PlaceholderVerticalAlign.TextBottom
+
+      if (width > lineHeightPx) {
+         val lineSize = (width / lineHeightPx).toInt() + 1
+         repeat(lineSize) { builder.appendLine() }
+         builder.appendInlineContent(id = src)
+         builder.appendLine()
+         placeholderVerticalAlign = PlaceholderVerticalAlign.Bottom
+      } else {
+         builder.appendInlineContent(id = src)
+      }
+
+      val size = density.run { width.toSp() }
+      addImgContent(
+         src = src,
+         alt = element.attr("alt"),
+         placeholder = Placeholder(
+            width = size,
+            height = size,
+            placeholderVerticalAlign = placeholderVerticalAlign,
+         )
+      )
+   }
+
+   protected abstract fun addImgContent(
+      src: String,
+      alt: String,
+      placeholder: Placeholder,
+   )
+}
