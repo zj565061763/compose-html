7e34f93e6b4698e6e4c1faea12160ebdee86002a
zjun <565061763@qq.com>
2024-11-28 13:42:36 +0800

    Tag_li

--- lib/src/main/java/com/sd/lib/compose/html/tags/Tag_li.kt
+++ lib/src/main/java/com/sd/lib/compose/html/tags/Tag_li.kt
@@ -2,13 +2,15 @@ package com.sd.lib.compose.html.tags
 
 import androidx.compose.ui.text.AnnotatedString
 import org.jsoup.nodes.Element
-import org.jsoup.nodes.TextNode
 
 class Tag_li(
-   private val dot: String = "• ",
+   private val dot: String = "•",
+   private val indent: String = " ",
 ) : TagBlock() {
-   override fun elementText(builder: AnnotatedString.Builder, element: Element, textNode: TextNode) {
-      builder.append(dot)
-      super.elementText(builder, element, textNode)
+   override fun elementStart(builder: AnnotatedString.Builder, element: Element): Boolean {
+      return super.elementStart(builder, element).also {
+         builder.append(dot)
+         builder.append(indent)
+      }
    }
 }
